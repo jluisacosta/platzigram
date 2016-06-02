9cfad52889e871705accf2dc2d6d0497a6e907be
José Luis Acosta Zamora <jluis.acosta@outlook.com>
2016-06-02 11:25:05 -0500

    Like functionality in picture card was implemented

--- public/app.js
+++ public/app.js
@@ -2336,23 +2336,41 @@ module.exports = function layout(content) {
 },{"yo-yo":8}],20:[function(require,module,exports){
 var yoyo = require('yo-yo');
 
-module.exports = function (picture) {
-  return yoyo`<div class="card">
-    <div class="card-image">
-      <img class="activator" src="${ picture.url }">
-    </div>
-    <div class="card-content">
-      <a href="/user/${ picture.user.username }" class="card-title">
-        <img src="${ picture.user.avatar }" class="avatar"/>
-        <span class="username">${ picture.user.username }</span>
-      </a>
-      <small class="right time">Hace 1 día</small>
-      <p>
-        <a class="left" href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-        <span class="left likes">${ picture.likes } Me Gusta</span>
-      </p>
-    </div>
-  </div>`;
+module.exports = function pictureCard(picture) {
+  var element;
+
+  function render(renderPicture) {
+    return yoyo`<div class="card ${ renderPicture.liked ? 'liked' : 'disliked' }">
+      <div class="card-image">
+        <img class="activator" src="${ renderPicture.url }">
+      </div>
+      <div class="card-content">
+        <a href="/user/${ renderPicture.user.username }" class="card-title">
+          <img src="${ renderPicture.user.avatar }" class="avatar"/>
+          <span class="username">${ renderPicture.user.username }</span>
+        </a>
+        <small class="right time">Hace 1 día</small>
+        <p>
+          <a class="left" href="#" onclick=${ like.bind(null, true) }><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+          <a class="left" href="#" onclick=${ like.bind(null, false) }><i class="fa fa-heart" aria-hidden="true"></i></a>
+          <span class="left likes">${ renderPicture.likes } Me Gusta</span>
+        </p>
+      </div>
+    </div>`;
+  }
+
+  function like(liked) {
+    var newElement;
+
+    picture.liked = liked;
+    picture.likes += liked ? 1 : -1;
+    newElement = render(picture);
+    yoyo.update(element, newElement);
+    return false;
+  }
+
+  element = render(picture);
+  return element;
 };
 
 },{"yo-yo":8}],21:[function(require,module,exports){
--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,22 +1,40 @@
 var yoyo = require('yo-yo');
 
-module.exports = function (picture) {
-  return yoyo
+module.exports = function pictureCard(picture) {
+  var element;
 
-  `<div class="card">
-    <div class="card-image">
-      <img class="activator" src="${picture.url}">
-    </div>
-    <div class="card-content">
-      <a href="/user/${picture.user.username}" class="card-title">
-        <img src="${picture.user.avatar}" class="avatar"/>
-        <span class="username">${picture.user.username}</span>
-      </a>
-      <small class="right time">Hace 1 día</small>
-      <p>
-        <a class="left" href="#"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-        <span class="left likes">${picture.likes} Me Gusta</span>
-      </p>
-    </div>
-  </div>`;
+  function render(renderPicture) {
+    return yoyo
+
+    `<div class="card ${renderPicture.liked ? 'liked' : 'disliked'}">
+      <div class="card-image">
+        <img class="activator" src="${renderPicture.url}">
+      </div>
+      <div class="card-content">
+        <a href="/user/${renderPicture.user.username}" class="card-title">
+          <img src="${renderPicture.user.avatar}" class="avatar"/>
+          <span class="username">${renderPicture.user.username}</span>
+        </a>
+        <small class="right time">Hace 1 día</small>
+        <p>
+          <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+          <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
+          <span class="left likes">${renderPicture.likes} Me Gusta</span>
+        </p>
+      </div>
+    </div>`;
+  }
+
+  function like(liked) {
+    var newElement;
+
+    picture.liked = liked;
+    picture.likes += liked ? 1 : -1;
+    newElement = render(picture);
+    yoyo.update(element, newElement);
+    return false;
+  }
+
+  element = render(picture);
+  return element;
 }
