d4f8cab9b03e8334cebf3305f2de885b210405da
José Luis Acosta Zamora <jluis.acosta@outlook.com>
2016-06-03 13:58:43 -0500

    Toggle show/hide for upload button when a file is selected

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -9,13 +9,13 @@ module.exports = function (pictures) {
   `<div class="container timeline">
     <div class="row">
       <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
-        <form enctype="multipart/form-data" class="form-upload">
+        <form enctype="multipart/form-data" class="form-upload" id="formUpload">
           <div id="fileName" class="fileUpload btn btn-flat cyan">
             <span><i class="fa fa-camera" aria-hidden="true"></i> ${translate.message('upload.text')}</span>
-            <input name="picture" id="file" type="file" class="upload"/>
+            <input name="picture" id="file" type="file" class="upload" onchange=${onChange}/>
           </div>
           <button id="btnUpload" type="submit" class="btn btn-flat cyan hide">${translate.message('upload.btn-text')}</button>
-          <button id="btnCancel" type="button" class="btn btn-flat red hide"><i class="fa fa-times" arria-hidden="true"></i></button>
+          <button id="btnCancel" type="button" class="btn btn-flat red hide" onclick=${cancel}><i class="fa fa-times" arria-hidden="true"></i></button>
         </form>
       </div>
     </row>
@@ -27,6 +27,21 @@ module.exports = function (pictures) {
       </div>
     </div>
   </div>`;
+
+  function toggleButtons() {
+    document.getElementById('fileName').classList.toggle('hide');
+    document.getElementById('btnUpload').classList.toggle('hide');
+    document.getElementById('btnCancel').classList.toggle('hide');
+  }
+
+  function cancel() {
+    toggleButtons();
+    document.getElementById('formUpload').reset();
+  }
+
+  function onChange() {
+    toggleButtons();
+  }
 
   return layout(element);
 }
