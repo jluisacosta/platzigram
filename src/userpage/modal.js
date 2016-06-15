bcf492af6b0374ed30dbaa7347c797c3e1866155
José Luis Acosta Zamora <jluis.acosta@outlook.com>
2016-06-14 23:22:42 -0500

    Picture modal design added and adapted for tablet and desktop

--- src/userpage/modal.js
+++ src/userpage/modal.js
@@ -1,4 +1,5 @@
 var yoyo = require('yo-yo');
+var translate = require('../translate');
 
 module.exports = function (photoData) {
   console.log(photoData);
@@ -6,8 +7,15 @@ module.exports = function (photoData) {
 
   `<div id="picture-modal" class="modal">
     <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>${photoData.username}</p>
+      <div>
+        <img class="responsive-img" src="${photoData.src}"/>
+      </div>
+      <div>
+        <img class="circle responsive-img" src="${photoData.avatar}"/>
+        <p><a href="/${photoData.username}">${photoData.username}</a></p>
+        <hr>
+        <p><i class="fa fa-heart" aria-hidden="true"></i> <span>${translate.message('likes', { likes: photoData.likes })}</span></p>
+      </div>
     </div>
   </div>`;
 }
